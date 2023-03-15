78ea7d1ce7e2fa842d35b2410b6bbae69af79884
Daniel Briskin <daniel.briskin@gmail.com>
2023-03-15 15:16:24 +0900

    delete .ts files

--- runtime/crypto/crypto.ts
+++
@@ -1,7 +0,0 @@
-// The mutable export is the whole key to the adapter architecture.
-
-// eslint-disable-next-line import/no-mutable-exports
-export let crypto: Crypto;
-export function setCrypto(providedCrypto: Crypto) {
-  crypto = providedCrypto;
-}
